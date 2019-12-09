d73fae2701561cb7c937a97f1965166cf43cf69c
Omar Caraballo <21061@columbushs.com>
2019-12-09 13:46:19 -0500

    yes

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,32 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health,strength){
+        this.health = health;
+        this.strength = strength;
+    }
+    attack(){
+        return this.strength;
+    }
+
+receiveDamage (damage) {
+    this.health = this.health - damage
+}
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor (name, health, strength){
+        super(health,strength);
+        
+    }
+}
+    
+
+    
+
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {}
 
 // War
 class War {}
