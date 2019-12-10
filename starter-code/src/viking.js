417ae0e7ab0e682a2be77e15f0f0dc7704c12330
Omar Caraballo <21061@columbushs.com>
2019-12-10 09:40:55 -0500

    WAR

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -17,7 +17,23 @@ receiveDamage (damage) {
 class Viking extends Soldier {
     constructor (name, health, strength){
         super(health,strength);
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
         
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage;
+        if (this.health <= 0){
+            return  `${this.name} has died in act of combat`;
+        }
+        else {
+            return `${this.name} has received ${damage} points of damage`;
+        }
+        
+    }
+    battleCry(){
+        return "Odin Owns You All!";
     }
 }
     
@@ -26,7 +42,36 @@ class Viking extends Soldier {
 
 
 // Saxon
-class Saxon extends Soldier {}
+class Saxon extends Soldier {
+    constructor (health, strength){
+        super(health, strength)
+        this.health = health;
+        this.strength = strength;
+    }
+    receiveDamage(damage){
+        this.health = this.health - damage;
+        if (this.health <= 0){
+            return  `A Saxon has died in combat`;
+        }
+        else {
+            return `A Saxon has received ${damage} points of damage`;
+            
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+    addViking(object){
+        this.vikingArmy.push(object)
+    }
+    addSaxon(object){
+        this.saxonArmy.push(object)
+
+    
+}
+}
